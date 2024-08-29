dbf3e1dea30fba3a13cee4ac064fed2953b2602b
Amanpreet Singh <amanpreet.singh@hotmail.com>
2024-08-29 19:02:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
-# -----------------
+# ------------------
 
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)       # top_k_results: number of documents
 arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
